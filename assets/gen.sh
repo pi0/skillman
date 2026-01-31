970f757c53697d1d7b3e158bfed4167da0f1786c
Pooya Parsa <pooya@pi0.io>
2026-01-31 23:21:45 +0100

    update gen.sh

--- assets/gen.sh
+++ assets/gen.sh
@@ -9,11 +9,13 @@ rm -rf "$TMP_DIR" && mkdir -p "$TMP_DIR" && cd "$TMP_DIR"
 cleanup() { rm -rf "$TMP_DIR"; }
 trap cleanup EXIT
 
+SKILLMAN="node ../../src/cli.ts"
+
 # https://github.com/pamburus/termframe
 
-# termframe -o "$SCRIPT_DIR/add.svg" --padding 2 -H auto --title "npx skillman add" -- pnpx skillman add skills.sh/vercel-labs/skills/find-skills anthropics/skills:skill-creator
+termframe -o "$SCRIPT_DIR/add.svg" --padding 2 -H auto --title "npx skillman add" -- $SKILLMAN add skills.sh/vercel-labs/skills/find-skills anthropics/skills:skill-creator
 
-# termframe -o "$SCRIPT_DIR/install.svg" --padding 2 -H auto --title "npx skillman" -- pnpx skillman
+termframe -o "$SCRIPT_DIR/install.svg" --padding 2 -H auto --title "npx skillman" -- $SKILLMAN
 
-termframe -o "$SCRIPT_DIR/detect.svg" --padding 2 -H auto --title "npx skillman detect" -- node ../../src/cli.ts detect
+termframe -o "$SCRIPT_DIR/detect.svg" --padding 2 -H auto --title "npx skillman detect" -- $SKILLMAN detect
 
